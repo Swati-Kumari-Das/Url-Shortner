29aafafdf026682f0f6611099393b783b3d34647
Swati-Kumari-Das <swatikumaridas1111@gmail.com>
2025-06-01 00:54:32 +0530

    added frontend

--- Backend/src/services/short_url.service.js
+++ Backend/src/services/short_url.service.js
@@ -1,7 +1,6 @@
 import { generateNanoId } from "../utils/helper.js";
 import urlSchema from "../models/short_url.model.js"
 import { saveShortUrl } from "../dao/short_url.js";
- 
 export const createShortUrlWithoutUser=async (url)=>{
  
     const shortUrl= await generateNanoId(7);
