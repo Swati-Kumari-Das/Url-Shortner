628af57324329fa3ec3600d7f4cffdde958837d3
Swati-Kumari-Das <swatikumaridas1111@gmail.com>
2025-06-01 16:37:59 +0530

    Remove frontend submodule

---
+++ Frontend/src/components/UrlForm.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+function UrlForm() {
+
+  const [url, setUrl] = useState("http://www.google.com")
+  const [shortUrl,setShortUrl]=useState("")
+  const [copied,setCopied]=useState(false)
+  const handleSubmit = async()=>{
+        const {data}=await axios.post("http://localhost:3000/api/create",{url})
+        setShortUrl(data);
+        // console.log(data);
+  }
+
+  const handleCopy=()=>{
+    navigator.clipboard.writeText(shortUrl);
+    setCopied(true);
+
+    //reset the copied state after 2 seconds 
+    setTimeout(()=>{
+        setCopied(false);
+    },2000);
+  }
+//   console.log(url)
+//   const [shortUrl, setShortUrl] = useState('')
+//   const [loading, setLoading] = useState(false)
+//   const [error, setError] = useState('')
+
+  return (
+    <div className="space-y-4">
+    <div>
+      <label htmlFor="url" className="block text-sm font-medium text-gray-700 mb-1">
+        Enter your long URL
+      </label>
+      <input
+        type="url"
+        id="url"
+        value={url} //two way binding 
+        onInput={(event)=>setUrl(event.target.value)}
+       
+        placeholder="https://example.com/very/long/url"
+        required
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
+    
+    <button  onClick={ handleSubmit}
+      type="submit"
+     
+      className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50"
+    >
+    Shorten URL
+    </button>
+      {/* {error && (
+          <div className="mt-4 p-3 bg-red-100 border border-red-400 text-red-700 rounded">
+            {error}
+          </div>
+        )} */}
+        
+        {shortUrl && (
+          <div className="mt-6">
+            <h2 className="text-lg font-medium text-gray-800 mb-2">Your shortened URL:</h2>
+            <div className="flex items-center">
+              <input
+                type="text"
+                readOnly
+                value={shortUrl}
+                className="flex-1 p-2 border border-gray-300 rounded-l-md bg-gray-50"
+              />
+              <button
+              onClick={handleCopy}
+              className={`px-4 py-2 rounded-r-md transition-colors duration-200 ${
+                copied 
+                  ? "bg-green-500 text-white hover:bg-green-600" 
+                  : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+              }`}
+            >
+              {copied ? "Copied!" : "Copy"}
+            </button>
+            </div>
+          </div>
+        )}
+  </div>
+  )
+}
+
+export default UrlForm
